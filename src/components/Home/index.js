070d683ac45dce37209ae211b45890a7d8706d18
Rohan <mistry.rohan2003@gmail.com>
2024-06-11 20:39:28 -0400

    Added personal logo to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-r.png'
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo'
 import './index.scss';
 
 const Home = () => {
@@ -35,6 +36,7 @@ const Home = () => {
                 <h2>Frontend Developer / Other stuff add later</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
+            <Logo />
         </div>
     );
 }
